4d0f659db39711b58153855cade38fa772051fcf
Maciej Preis <maciej.preis@onet.eu>
2020-07-10 17:02:05 +0200

    Add basic hero image

---
+++ src/components/Claim.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="claimWrapper">
+    <h1 class="claim">SPACER</h1>
+    <p class="subclaim">
+      Begin your journey thorugh our amazing galaxy, and discover
+    </p>
+    <p class="subclaim">
+      Type anything space-related to start.
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Claim',
+};
+</script>
+<style lang="scss" scoped>
+.claimWrapper{
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.claim {
+  font-size: 50px;
+}
+
+.subclaim{
+  font-size: 13px;
+  margin-top: 20px 0 0 0;
+
+}
+
+</style>
