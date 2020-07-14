a8c8b077ba410634cdebf7f6fa057b5cc938872f
Maciej Preis <maciej.preis@onet.eu>
2020-07-14 16:19:56 +0200

    Koncowe zmiany

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
-  <div class="wrapper">
-    <HeroImage />
-    <Claim />
-    <SearchInput v-model="searchValue" @input="handleInput" />
+  <div :class="[{ flexStart: step === 1 }, 'wrapper']">
+    <transition name="slide">
+      <img src="./assets/logo.svg" class="logo" v-if="step === 1">
+    </transition>
+    <transition name="fade">
+      <HeroImage v-if="step === 0" />
+    </transition>
+    <Claim v-if="step === 0" />
+    <SearchInput v-model="searchValue" @input="handleInput" :dark="step === 1" />
+    <div class="results" v-if="results && !loading && step ===1">
+      <Item
+      v-for="item in results"
+      :item="item"
+      :key="item.data[0].nasa_id"
+      @click.native="handleModalOpen(item)"/>
+    </div>
+    <Modal v-if="modalOpen" :item="modalItem" @closeModal="modalOpen = false"/>
   </div>
 </template>
 <script>
@@ -11,6 +24,8 @@ import debounce from 'lodash.debounce';
 import Claim from '@/components/Claim.vue';
 import SearchInput from '@/components/SearchInput.vue';
 import HeroImage from '@/components/HeroImage.vue';
+import Item from '@/components/Item.vue';
+import Modal from '@/components/Modal.vue';
 
 const API = 'https://images-api.nasa.gov/search';
 
@@ -20,42 +35,75 @@ export default {
     HeroImage,
     Claim,
     SearchInput,
+    Item,
+    Modal,
   },
   data() {
     return {
+      modalOpen: false,
+      modalItem: null,
+      loading: false,
+      step: 0,
       searchValue: '',
       results: [],
     };
   },
   methods: {
+    handleModalOpen(item) {
+      this.modalOpen = true;
+      this.modalItem = item;
+    },
     handleInput: debounce(function () {
+      this.loading = true;
       console.log(this.searchValue);
       axios.get(`${API}?q=${this.searchValue}&media_type=image`)
         .then((response) => {
           this.results = response.data.collection.items;
+          this.loading = false;
+          this.step = 1;
         })
         .catch((error) => {
           console.log(error);
         });
-    }, 500),
+    }, 400),
   },
 };
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;800&display=swap');
 
 * {
   box-sizing: border-box;
+  -webkit-font-smoothing: antialised;
+  -moz-osx-font-smoothing: grayscale;
 }
 
-body{
+body {
   font-family: 'Montserrat', sans-serif;
   margin: 0;
   padding: 0;
 }
 
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .4s ease;
+}
+
+.fade-enter, .fade-leave-to{
+  opacity: 0;
+}
+
+.slide-enter-active, .slide-leave-active {
+  transition: margin-top .4s ease;
+}
+
+.slide-enter, .slide-leave-to{
+  margin-top: -50px;
+}
+
 .wrapper {
   margin: 0;
+  position:relative;
   width: 100%;
   min-height: 100vh;
   padding: 30px;
@@ -63,5 +111,26 @@ body{
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  &.flexStart {
+    justify-content: flex-start;
+  }
 }
+
+.logo{
+  position: absolute;
+  top: 30px;
+}
+
+.results {
+  margin-top: 50px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+}
+
 </style>
--- src/components/HeroImage.vue
+++ src/components/HeroImage.vue
@@ -1,6 +1,7 @@
 <template>
-<div class="heroImage" />
+  <div class="heroImage" />
 </template>
+
 <script>
 export default {
   name: 'HeroImage',
@@ -8,26 +9,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.heroImage{
-    width: 100%;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    z-index: -1;
-    background-image: url('../assets/heroimage.jpg');
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: 80% 0%;
-}
+  .heroImage{
+      width: 100%;
+      height: 100vh;
+      position: fixed;
+      top: 0;
+      z-index: -1;
+      background-image: url('../assets/heroimage.jpg');
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: 80% 0%;
+  }
 
-.heroImage::after {
-    content: '';
-    background: rgba(0,0,0, .2);
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 1;
-    width: 100%;
-    height: 100vh;
-}
+  .heroImage::after {
+      content: '';
+      background: rgba(0,0,0, .2);
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 1;
+      width: 100%;
+      height: 100vh;
+  }
 </style>
